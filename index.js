a8fd2dbbca6132ba83d8bf9c65c74790e8ab2276
TriMadon <mehammed6789@gmail.com>
2023-07-25 19:05:13 +0300

    Refactor number check into a separate function

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 function add(...nums) {
-    for (const num of nums) {
-        if ((typeof num) !== "number") {
-            return num + " is an invalid input";
-        }
-    }
-    return nums.reduce((sum, num) => sum + num);
+	if (checkInvalidNum(nums)) return "Addition Error";
+	return nums.reduce((sum, num) => sum + num);
 }
 
 function subtract(...nums) {
-    for (const num of nums) {
-        if ((typeof num) !== "number") {
-            return num + " is an invalid input";
-        }
-    }
-    return nums.reduce((sum, num) => sum - num);
+	if (checkInvalidNum(nums)) return "Subtraction Error";
+	return nums.reduce((sum, num) => sum - num);
 }
 
 function multiply(...nums) {
-    for (const num of nums) {
-        if ((typeof num) !== "number") {
-            return num + " is an invalid input";
-        }
-    }
-    return nums.reduce((sum, num) => sum * num);
+	if (checkInvalidNum(nums)) return "Multiplication Error";
+	return nums.reduce((sum, num) => sum * num);
 }
 
 function divide(...nums) {
-    for (const num of nums) {
-        if ((typeof num) !== "number") {
-            return num + " is an invalid input";
-        }
-    }
-    return nums.reduce((sum, num) => sum / num);
+	if (checkInvalidNum(nums)) return "Division Error";
+	return nums.reduce((sum, num) => sum / num);
+}
+
+function checkInvalidNum(...nums) {
+	for (const num of nums) {
+		if (typeof num !== "number") {
+			console.log(num + " is an invalid input");
+			return true;
+		}
+	}
+	return false;
 }
