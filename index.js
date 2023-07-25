309c3dcda2afb5f72687cc71c56e63d0be3c1ed5
TriMadon <mehammed6789@gmail.com>
2023-07-25 18:54:47 +0300

    Add "multiply" function

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ function subtract(...nums) {
     return nums.reduce((sum, num) => sum - num);
 }
 
+function multiply(...nums) {
+    for (const num of nums) {
+        if ((typeof num) !== "number") {
+            return num + " is an invalid input";
+        }
+    }
+    return nums.reduce((sum, num) => sum * num);
+}
 
 
 
