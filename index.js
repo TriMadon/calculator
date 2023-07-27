daed299bb4c849a3fcbae279fa36c31e62f0db4f
TriMadon <mehammed6789@gmail.com>
2023-07-27 11:38:29 +0300

    Add CSS and interactivity

--- index.js
+++ index.js
@@ -33,14 +33,59 @@ function operate(num1, num2, operator) {
 		case "+":
 			return add(num1, num2);
 		case "-":
+		case "−":
 			return subtract(num1, num2);
 		case "*":
+		case "×":
+		case "x":
 			return multiply(num1, num2);
 		case "/":
+		case "÷":
 			return divide(num1, num2);
 		default:
 			return operator + " is an invalid operator";
 	}
 }
 
-console.log(operate(3, 3, "+"));
+console.log(operate(3, 3, "/"));
+
+// #region CSS interactivity
+
+// Make input textarea expand automatically to text
+const inputField = document.querySelector(".input");
+inputField.addEventListener("input", function () {
+	this.style.height = "auto";
+	this.style.height = this.scrollHeight + "px";
+});
+
+// Clicking anywhere within this container 
+// will only activate the input textarea
+const textFieldContainer = document.querySelector(".text-field-container");
+textFieldContainer.addEventListener("click", function () {
+	document.querySelector(".input").focus();
+});
+
+// Create a custom cursor to track its location on page
+const cursor = document.getElementById("customCursor");
+document.addEventListener("mousemove", (e) => {
+	cursor.setAttribute(
+		"style",
+		"top:" + (e.pageY - 5) + "px; left:" + (e.pageX - 5) + "px;"
+	);
+});
+
+// Increase font size based on the distance
+// from the custom cursor to the button
+const buttons = document.querySelectorAll("button");
+document.addEventListener("mousemove", (e) => {
+	buttons.forEach((button) => {
+		let rect = button.getBoundingClientRect();
+		let x = e.clientX - (rect.left + rect.width / 2);
+		let y = e.clientY - (rect.top + rect.height / 2);
+		let distance = Math.sqrt(x * x + y * y) - 100;
+		let scale = Math.min(100, Math.max(0, 100 - distance)) / 140;
+		button.style.fontSize = `${scale + 1.2}em`;
+	});
+});
+
+// #endregion
