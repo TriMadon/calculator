00dfacd19e2ee756bd6360aa11cba58223219b26
TriMadon <mehammed6789@gmail.com>
2023-08-04 10:44:24 +0300

    Fix modulus keyboard bug

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const allowedKeysMap = {
 	"+": "+",
 	"-": "−",
 	"^": "^",
+	"%": "%",
 };
 const allowedOperatorKeys = ["*", "/", "+", "-", "^", "%"];
 const operators = ["×", "÷", "+", "−", "^", "%"];
